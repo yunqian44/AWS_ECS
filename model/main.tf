0b88b0c17ee47826842bc0b0e3216cc06c1bbf49
yunqian44 <yunqian8@live.com>
2021-06-01 13:53:53 +0800

    modify ecs file path

--- model/main.tf
+++ model/main.tf
@@ -96,36 +96,6 @@ module "ecs_fargate" {
   }
 }
 
-resource "aws_iam_role" "default" {
-  name               = "ecs-task-execution-for-ecs-fargate"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
-}
-
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_policy" "default" {
-  name   = aws_iam_role.default.name
-  policy = data.aws_iam_policy.ecs_task_execution.policy
-}
-
-resource "aws_iam_role_policy_attachment" "default" {
-  role       = aws_iam_role.default.name
-  policy_arn = aws_iam_policy.default.arn
-}
-
-data "aws_iam_policy" "ecs_task_execution" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
 module "alb" {
   source                     = "../../modules/elb"
   name                       = "ecs-fargate"
